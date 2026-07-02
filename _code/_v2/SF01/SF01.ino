b891c352998088418cdc0d89b39068b8f14fc095
Olivier Bienz <58851935+lan-ensad@users.noreply.github.com>
2026-07-02 10:02:34 +0200

    Update SF01.ino

--- _code/_v2/SF01/SF01.ino
+++ _code/_v2/SF01/SF01.ino
@@ -20,9 +20,9 @@
 #define MS3 5
 #define STEP 3
 #define DIR 2
-#define MAXSPEED 1000 //Set maximum speed
+#define MAXSPEED 600 //Set maximum speed
 #define ACCEL 450 //Set acceleration
-#define DEFAULT_STP 340
+#define DEFAULT_STP 170
 AccelStepper stepper(AccelStepper::DRIVER, STEP, DIR);
 
 
@@ -31,7 +31,7 @@ AccelStepper stepper(AccelStepper::DRIVER, STEP, DIR);
 //----------------------------------------------------------------------------
 int pause_standard = 1618;
 int stp = DEFAULT_STP;  //nb de steps/flap
-int init_flaps = 10; //nb de flaps passés lors du démarrage
+int init_flaps = 40; //nb de flaps passés lors du démarrage
 int timing = pause_standard; //temps entre deux flaps → l.65-69
 
 
@@ -47,9 +47,9 @@ void setup() {
   pinMode(MS3, OUTPUT);
 
   digitalWrite(EN, 0);  //0=ON ; 1=OFF
-  digitalWrite(MS1, 0);
-  digitalWrite(MS2, 0);
-  digitalWrite(MS2, 0);
+  digitalWrite(MS1, 1);
+  digitalWrite(MS2, 1);
+  digitalWrite(MS3, 0);
 
   stepper.setMaxSpeed(MAXSPEED);
   stepper.setAcceleration(ACCEL);
